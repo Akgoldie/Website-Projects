18c5f0070ee0f4fd70cebbeb6342f6396d13dab4
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-02-05 00:20:50 +0530

    Area of rectangle

---
+++ Code_1/JS1_Code_JS.js
@@ -0,0 +1,6 @@
+function area(){
+    let Length=document.getElementById('Length').value;
+    let Breadth=document.getElementById('Breadth').value;
+    let result=Length*Breadth;
+    document.getElementById('result').value=result
+}
